fcf426bd822dd183cbc949fddab5d8745da7d127
jjkuang <sam.joey.kuang@gmail.com>
2020-02-05 16:35:31 -0500

    update. no pics added

--- Q1.py
+++ Q1.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
 
 # function for the derivative of state vector
 def f(x):
@@ -12,8 +13,7 @@ def f(x):
 
 
 # Implementation of Euler's method for given system
-def euler():
-	dt = 0.05
+def euler(dt):
 	times = []
 	trajectory = []
 	t = 0
@@ -32,4 +32,44 @@ def euler():
 	plt.tight_layout()
 	plt.show()
 
-euler()
+
+def rk4_update(dt):
+	times = np.arange(0,10,dt)
+	x_trajectory = []
+	t = 0
+	x = np.array([1, 0])
+
+	for t in times:  
+		s1 = x + dt * f(x)
+		s2 = s1 + 0.5 * dt * f(s1)
+		s3 = s2 + 0.5 * dt * f(s2)
+		s4 = s3 + dt * f(s3)
+		x = x + dt * (s1 + (2*s2) + (2*s3) + s4)/6
+		x_trajectory.append(x)
+
+	plt.plot(times, x_trajectory)	
+
+	plt.xlabel('Time (s)')
+	plt.ylabel('State')
+	plt.tight_layout()
+	plt.show()
+
+
+# callable function for solve_ivp method 
+# includes t as parameter
+def g(t,x):
+	return np.matmul([[0, -1], [1, 0]], x)
+
+def use_ivp():
+	sol = solve_ivp(g, [0, 10], [1, 0], max_step=.05)
+	plt.plot(sol.t, sol.y.T)
+	plt.xlabel('Time (s)')
+	plt.show()
+
+
+
+dts = [1, 0.5, 0.25, 0.125, 0.0625, 0.0375, \
+		0.01875, 0.009285, 0.0046425]
+
+for i in range(len(dts)):
+	euler(dts(i))
--- Q2.py
+++ Q2.py
@@ -7,22 +7,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-a, b =1000, 1
+a, b = 1000, 1
 alpha = 20000
 p0 = 2000
 g = 9.807
 m = 220
 
 def f(x):
-    return [
+    return np.array([
         alpha * (x[1] + b*(p0-x[0])/(x[0]+a)),
         g-x[0]/m,
         x[1]
-    ]
+    ])
 
 def expl_trape():
-    dt = 0.001
-    time = np.arrange (0, 2, dt)
+    dt = 0.0046425
+    time = np.arange (0, 2, dt)
     x= np.array([0,0,0])
     x_trajectory = []
     for t in time:  
@@ -33,9 +33,15 @@ def expl_trape():
 
 
     x_trajectory = np.array(x_trajectory)
-    plt.plot(times, x_trajectory)
-    plt.xlabel('Time (s)')
-    plt.ylabel('State')
+    plt.subplot(3,1,1)
+    plt.plot(time, x_trajectory[:,0] )
+    plt.ylabel('Tension (N)')
+    plt.subplot(3,1,2)
+    plt.plot(time, x_trajectory[:,1])
+    plt.ylabel('Velocity (m/s)')
+    plt.subplot(3,1,3)
+    plt.plot(time,x_trajectory[:,2])
     plt.show()
+
 
 expl_trape()
