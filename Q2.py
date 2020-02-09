d5babb06587dd98a903d7685fe3b7443715ba93d
jjkuang <sam.joey.kuang@gmail.com>
2020-02-09 11:48:25 -0500

    update

--- Q2.py
+++ Q2.py
@@ -28,35 +28,36 @@ def f(x):
 
 # Function created for explicit trapezoid integration method
 def expl_trap(dt):
-	
-	time = np.arange(0, 2, dt)  # time range for 2 secconds
-	x = np.array([0, 0, 0])  # initial state is x = [0, 0 , 0]
-	x_trajectory = []
+    time = np.arange(0, 2, dt)  # time range for 2 secconds
+    x = np.array([0, 0, 0])  # initial state is x = [0, 0 , 0]
+    x_trajectory = []
 
-	# implement the explicit trapezoid integration method
-	for t in time:
-		xa = f(x + dt * x)
-		xb = f(x + dt * xa)
-		x = x + 0.5 * dt * (xa + xb)
-		x_trajectory.append(x)
+    # implement the explicit trapezoid integration method
+    for t in time:
+	    xa = f(x + dt * x)
+	    xb = f(x + dt * xa)
+	    x = x + 0.5 * dt * (xa + xb)
+	    x_trajectory.append(x)
 
-	# Plot the state vs time graphs for the three dynamic equations
-	x_trajectory = np.array(x_trajectory)
-	plt.subplot(3, 1, 1)
-	plt.plot(time, x_trajectory[:, 0])
-	plt.ylabel('Tension (N)')
-	plt.subplot(3, 1, 2)
-	plt.plot(time, x_trajectory[:, 1])
-	plt.ylabel('Velocity (m/s)')
-	plt.subplot(3, 1, 3)
-	plt.plot(time, x_trajectory[:, 2])
-	plt.show()
+    # Plot the state vs time graphs for the three dynamic equations
+    x_trajectory = np.array(x_trajectory)
+    plt.subplot(3, 1, 1)
+    plt.plot(time, x_trajectory[:, 0])
+    plt.ylabel('Tension (N)')
+    plt.subplot(3, 1, 2)
+    plt.plot(time, x_trajectory[:, 1])
+    plt.ylabel('Velocity (m/s)')
+    plt.subplot(3, 1, 3)
+    plt.plot(time, x_trajectory[:, 2])
+    plt.show()
 
 
 def find_timestep():
 
-    dts = [1, 0.5, 0.25, 0.125, 0.0625, 0.03125, \
-          0.015625, 0.0078125, 0.00390625, 0.001953125]
+    # dts = [1, 0.5, 0.25, 0.125, 0.0625, 0.03125, \
+    #       0.015625, 0.0078125, 0.00390625, 0.001953125]
+    dts = [0.0009765625]
+    # dt2 = [0.00390625, 0.001953125]
 
     for i in range(len(dts)):
         plt.figure(i)
